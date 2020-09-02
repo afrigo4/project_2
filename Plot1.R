5c357fc0e10993282eeaec59ad3e99ccf4795e77
Anthony Frigo <anthony.frigo@phmc.com>
2020-09-02 18:24:27 -0500

    updated once again

--- Plot1.R
+++ Plot1.R
@@ -12,4 +12,4 @@ aggTotals <- aggregate(Emissions ~ year, NEI, sum)
 
 png("plot1.png", width = 480, height = 480, units = "px", bg = "transparent")
 
-barplot((aggTotals$Emissions)/10^6, names.arg=aggTotals$year, xlab="Year", ylab="PM2.5 Emissions (10^6 Tons)", main="Total PM2.5 Emissions From All US Sources")
+barplot((aggTotals$Emissions)/10^6, names.arg = aggTotals$year, xlab = "Year", ylab = "PM2.5 Emissions (10^6 Tons)", main = "Total PM2.5 Emissions From All US Sources")
