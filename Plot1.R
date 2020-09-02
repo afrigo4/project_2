827313683c73676263b76f5abe08afa859226311
Anthony Frigo <anthony.frigo@phmc.com>
2020-09-02 18:07:49 -0500

    first plots

---
+++ Plot1.R
@@ -0,0 +1,15 @@
+setwd("C:/Users/afrigo/Desktop/Hopkins Data Science/Class 3")
+
+# Load the NEI & SCC data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#change back to correct folder
+setwd("C:/Users/afrigo/Desktop/Hopkins Data Science/Class 3/project_2")
+
+# Aggregate by sum the total emissions by year
+aggTotals <- aggregate(Emissions ~ year, NEI, sum)
+
+png("plot1.png", width = 480, height = 480, units = "px", bg = "transparent")
+
+barplot((aggTotals$Emissions)/10^6, names.arg=aggTotals$year, xlab="Year", ylab="PM2.5 Emissions (10^6 Tons)", main="Total PM2.5 Emissions From All US Sources")
