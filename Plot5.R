bb369e959c92465d74950232c96d4ad41f50b9bf
Anthony Frigo <anthony.frigo@phmc.com>
2020-09-02 18:12:49 -0500

    almost done

---
+++ Plot5.R
@@ -0,0 +1,24 @@
+setwd("C:/Users/afrigo/Desktop/Hopkins Data Science/Class 3")
+
+# Load the NEI & SCC data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+#change back to correct folder
+setwd("C:/Users/afrigo/Desktop/Hopkins Data Science/Class 3/project_2")
+
+# Gather the subset of the NEI data which corresponds to vehicles
+vehicles <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case = TRUE)
+vehiclesSCC <- SCC[vehicles, ]$SCC
+vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC, ]
+
+# Subset the vehicles NEI data to Baltimore's fip
+baltimoreVehiclesNEI <- vehiclesNEI[vehiclesNEI$fips == "24510", ]
+
+png("plot5.png", width = 480, height = 480, units = "px", bg = "transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(baltimoreVehiclesNEI, aes(factor(year), Emissions)) + geom_bar(stat = "identity", fill = "grey", width = 0.75) + theme_bw() +  guides(fill = FALSE) + labs(x = "year", y = expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + labs(title = expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
+
+print(ggp)
